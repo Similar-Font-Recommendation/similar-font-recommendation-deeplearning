7aa46aa183e182bc406967de6cb914039dc8a99f
jiae yoon <68904755+jiaeYoon@users.noreply.github.com>
2022-11-27 01:56:00 +0900

    chore : add img saving code and modify trim function
    trim 함수의 RGBA 방식의 픽셀 색깔 비교를 RGB 방식의 픽셀 색깔 비교로 수정하여 오류를 수정함

--- Text Rendering/trim_white space.py
+++ Text Rendering/trim_white space.py
@@ -1,13 +1,15 @@
 from PIL import Image
+import os
+
 def trim(img):
   pixels = img.load()
-
-  print (f"original: {img.size[0]} x {img.size[1]}")
+  # print (f"original: {img.size[0]} x {img.size[1]}")
+  
   xlist = []
   ylist = []
   for y in range(0, img.size[1]):
     for x in range(0, img.size[0]):
-      if pixels[x, y] != (255, 255, 255, 255):
+      if pixels[x, y] != (255, 255, 255):
         xlist.append(x)
         ylist.append(y)
   left = min(xlist)
@@ -16,9 +18,13 @@ def trim(img):
   bottom = max(ylist)
   
   img = img.crop((left, top, right, bottom))
-  print (f"cropped: {img.size[0]} x {img.size[1]}")
+  # print (f"cropped: {img.size[0]} x {img.size[1]}")
   return img
   
-img = Image.open("test_img.PNG")
-trimed_img = trim(img)
-trimed_img.show()
+imgs_path = "img_load_path"
+img_list = os.listdir(imgs_path)
+for img_name in img_list:
+  img = Image.open(imgs_path + img_name)
+  trimed_img = trim(img)
+  trimed_img.save("img_save_path" + img_name)
+# trimed_img.show()
